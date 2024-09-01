1671a13d96105c3047e1378406095063ae98d648
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-09-01 16:12:43 +0100

    installed initial project dependencies

---
+++ src/main/java/com/sirfeeky/dream_shops/DreamShopsApplication.java
@@ -0,0 +1,13 @@
+package com.sirfeeky.dream_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirfeeky/dream_shops/DreamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirfeeky.dream_shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
